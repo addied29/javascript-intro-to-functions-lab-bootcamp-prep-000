85fa066d98df1e851ebd0620394d75a48881d4f8
Addie Dean <addiedean@gmail.com>
2019-04-06 17:23:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,17 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('Hello'.toLowerCase())
 }
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
 
-function sayHiToGrandma (string) {
-  if ('I love you, Grandma.')
-  return ('I love you, too.')
-  if ('hello'.toLowerCase())
-  return ('I can\'t hear you.')
-  if ('HELLO'.toUpperCase())
-  return ('YES INDEED')
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
 }
