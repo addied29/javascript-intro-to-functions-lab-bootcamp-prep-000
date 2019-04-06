e359a6288973d9c55892c21ca7f12122fdac72d3
Addie Dean <addiedean@gmail.com>
2019-04-06 17:05:22 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('Hello'.toUpperCase())
+}
+function logWhisper(string) {
+  console.log('Hello'.toLowerCase())
+}
+
+function sayHiToGrandma (string) {
+  if ('I love you, Grandma.')
+  return ('I love you, too.')
+  if ('hello'.toLowerCase())
+  return ('I can\'t hear you.')
+  if ('HELLO'.toUpperCase())
+  return ('YES INDEED')
+}
